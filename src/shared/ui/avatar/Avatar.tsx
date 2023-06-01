fb6a912aa25ae5ed6a4b85d5cdb1fca83084debf
AlexRixten <rixten.09@gmail.com>
2023-06-01 12:00:00 +0300

    fix Avatar

--- src/shared/ui/avatar/Avatar.tsx
+++ src/shared/ui/avatar/Avatar.tsx
@@ -6,7 +6,7 @@ export const Avatar = () => {
 	return (
 		<div className="avatar">
 			<Image src={avatar} width={150} height={150} alt="avatar" draggable={false} />
-			<h4 className="title h4">Alexey Kharitonov</h4>
+			<h4 className="title h4">Aleksei Kharitonov</h4>
 			<p className="subtitle p">Frontend Developer</p>
 		</div>
 	);
